2e9c46863006fa8be973a56e532515137a553277
Joaquín Ignacio Barotto <joaquinbarotto@gmail.com>
2020-06-21 18:02:00 +0300

    Add __all__ to module

--- psyched/task/__init__.py
+++ psyched/task/__init__.py
@@ -1,6 +1,18 @@
 """This module exports all the Task classes."""
-from .docker_task import DockerTask  # noqa
-from .python_task import PythonTask  # noqa
-from .shell_task import ShellTask  # noqa
-from .task import (Task, _STATUS_FAILED, _STATUS_RUNNING, _STATUS_SCHEDULED,  # noqa
-                   _STATUS_SUCCEEDED, _STATUS_WAITING)  # noqa
+from .docker_task import DockerTask
+from .python_task import PythonTask
+from .shell_task import ShellTask
+from .task import (_STATUS_FAILED, _STATUS_RUNNING, _STATUS_SCHEDULED,
+                   _STATUS_SUCCEEDED, _STATUS_WAITING, Task)
+
+__all__ = [
+    'DockerTask',
+    'PythonTask',
+    'ShellTask',
+    'Task',
+    '_STATUS_FAILED',
+    '_STATUS_RUNNING',
+    '_STATUS_SCHEDULED',
+    '_STATUS_SUCCEEDED',
+    '_STATUS_WAITING'
+]
